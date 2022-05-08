07263852c4180faa15718e68e53f1bc24c858c38
xavi <x.carbonell1dawnuria2020@gmail.com>
2022-05-08 23:10:48 +0200

    Puesta a punto

---
+++ database/factories/NftFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Nft;
+use Faker\Generator as Faker;
+
+$factory->define(Nft::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
