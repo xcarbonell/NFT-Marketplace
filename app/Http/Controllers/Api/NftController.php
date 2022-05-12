e49528e4202372ae173380fb9bfdfc90bb8fcee4
xavi <x.carbonell1dawnuria2020@gmail.com>
2022-05-12 16:53:46 +0200

    auth added

--- app/Http/Controllers/Api/NftController.php
+++ app/Http/Controllers/Api/NftController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Nft;
 
 class NftController extends Controller
 {
@@ -15,6 +16,18 @@ class NftController extends Controller
     public function index()
     {
         //
+        $nfts = Nft::all();
+
+        if (count($nfts) == 0) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No nfts were found'
+            ], 200);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $nfts->toArray()
+        ], 200);
     }
 
     /**
@@ -47,6 +60,19 @@ class NftController extends Controller
     public function show($id)
     {
         //
+        $nft = Nft::where('id', $id)->get();
+
+        if (count($nft) == 0) {
+            return response()->json([
+                'success' => false,
+                'message' => 'NFT with id ' . $id . ' not found'
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $nft->toArray()
+        ], 200);
     }
 
     /**
@@ -57,7 +83,8 @@ class NftController extends Controller
      */
     public function edit($id)
     {
-        //
+        //retorna la vista para editar un NFT
+        return view();
     }
 
     /**
@@ -70,6 +97,32 @@ class NftController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $nft = Nft::find($id);
+
+        if (!$nft) {
+            return response()->json([
+                'success' => false,
+                'message' => 'NFT with id ' . $id . ' not found'
+            ], 200);
+        }
+
+        $validated = $request->validate([
+            'title' => 'required|max:50'
+        ]);
+
+        $nft->title = $validated['title'];
+
+        if (!$nft->update($validated)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'NFT with id ' . $id . ' can not be updated'
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => 'NFT updated'
+        ], 200);
     }
 
     /**
