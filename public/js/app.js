630baf8ad7495b297fbcfcd846f12687d2d182b2
iban <ibandediosblanco@hotmail.com>
2022-05-13 17:28:30 +0200

    1st commit

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/xavi/Apps/NFT-Marketplace/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/xavi/Apps/NFT-Marketplace/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/iban/ProjecteM12/NFT-Marketplace/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/iban/ProjecteM12/NFT-Marketplace/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
