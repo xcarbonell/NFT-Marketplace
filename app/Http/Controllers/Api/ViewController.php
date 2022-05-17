cfdc826cd681f10dfeab92dd9490a89debd525a2
iban <ibandediosblanco@hotmail.com>
2022-05-17 18:32:22 +0200

    view controller fix

--- app/Http/Controllers/Api/ViewController.php
+++ app/Http/Controllers/Api/ViewController.php
@@ -6,38 +6,56 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use App\Nft;
+use DB;
 
 class ViewController extends Controller
 {
     //pagina de inicio
     public function index()
     {
-        $usersSellingID = Nft::where('onStock', 1)->groupBy('user_id')->get(['user_id']);
+        /*$usersSellingID = Nft::where('onStock', 1)->groupBy('user_id')->limit(5)->get(['user_id']);
         $users = [];
         foreach ($usersSellingID as $u) {
-            $lista = User::find($u, ['id', 'name', 'photo'])->inRandomOrder()->limit(5);
-            $users[] = $lista[0];
-        }
+            $lista = User::inRandomOrder()->first();
+            $users[] = $lista;
+        }*/
 
-        if (count($users) == 0) {
+        $usersSellingID = Nft::where('onStock', 1)->groupBy('user_id')->get(['user_id']);
+        
+        $users = [];
+        foreach ($usersSellingID as $u) {
+            $lista = User::find($u, ['id', 'name', 'photo']);
+            $users[] = $lista;
+        }
+        shuffle($users);
+        //var_dump($users[0]);
+        
+        $randomUsers[] = $users[0][0];
+        $randomUsers[] = $users[1][0];
+
+        //dd($randomUsers);
+        if (count($randomUsers) == 0) {
             return response()->json([
                 'success' => false,
                 'message' => 'No sellers were found'
             ], 200);
         }
 
-        $categories = Nft::all()->groupBy('category')->inRandomOrder()->limit(5)->get(['id', 'category']);
+        $categories = Nft::all()->groupBy('category');
+        $randomCat = array_rand($categories->toArray(),3);
+        //dd($categories, $randomCat);
 
-        if (count($categories) == 0) {
+        if (count($randomCat) == 0) {
             return response()->json([
                 'success' => false,
                 'message' => 'No categories were found'
             ], 200);
         }
-
+        
         return response()->json([
             'success' => true,
-            'data' => compact(['users' => $users, 'categories' => $categories])
+            'categories' => $randomCat,
+            'users' => $randomUsers
         ], 200);
     }
 
