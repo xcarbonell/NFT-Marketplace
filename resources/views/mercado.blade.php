744a8f9910b45f8150ddfd1ee98b6acff470edf3
xcarbonell <x.carbonell1dawnuria2020@gmail.com>
2022-05-27 16:25:35 +0200

    Merge branch 'main' of https://github.com/xcarbonell/NFT-Marketplace into main

--- resources/views/mercado.blade.php
+++ resources/views/mercado.blade.php
@@ -3,22 +3,22 @@
     <div id="headermercado">Mercado</div>
     <div id="inventario">
     </div>
-<script>
-    //api/shops
-    const cardnft = document.getElementsByClassName("card-nft");
-    const button = document.getElementById("buttonbut");
-    const inventario = document.getElementById("inventario");
-    const getListNFT = async () => {
-        const response = await fetch('{{ env('APP_URL') }}'+":8000/api/shops")
-        .then(res => {
-            return res.json();
-        })
-        .then(data => data)
-        .catch(err => err)
-        console.log('{{ env('APP_URL') }}');
-        console.log(response.data);
-        response.data.map((nft) =>  {
-            inventario.innerHTML += `
+    <script>
+        //api/shops
+        const cardnft = document.getElementsByClassName("card-nft");
+        const button = document.getElementById("buttonbut");
+        const inventario = document.getElementById("inventario");
+        const getListNFT = async () => {
+            const response = await fetch('{{ env('APP_URL') }}' + ":8000/api/shops")
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => data)
+                .catch(err => err)
+            console.log('{{ env('APP_URL') }}');
+            console.log(response.data);
+            response.data.map((nft) => {
+                inventario.innerHTML += `
             <div class="card-nft" id="${nft.id}">
         <div class="card-image">
             <img src="{{ asset('storage/${nft.photo}') }}"></img>
@@ -38,20 +38,20 @@
         </div>
             
             `;
-        });
-        onClickCard();
-    }
+            });
+            onClickCard();
+        }
 
-    function onClickCard(){
-        for(let i = 0; i < cardnft.length-1; i++){
-        console.log("hola");
-        cardnft[i].addEventListener("click", (e) => {
-            console.log(e.target.parentElement.id);
-            window.location = '{{ env('APP_URL') }}'+":8000/nft"
-        });
-    }
-    }
-    window.onload = getListNFT();
-    console.log("Prueba");
-</script>
+        function onClickCard() {
+            for (let i = 0; i < cardnft.length - 1; i++) {
+                console.log("hola");
+                cardnft[i].addEventListener("click", (e) => {
+                    console.log(e.target.parentElement.id);
+                    window.location = '{{ env('APP_URL') }}' + ":8000/nft"
+                });
+            }
+        }
+        window.onload = getListNFT();
+        console.log("Prueba");
+    </script>
 @endsection
