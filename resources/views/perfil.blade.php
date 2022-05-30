60d9cf0dcc5bf9ec6d54eeec34897c02e5c30273
xcarbonell <x.carbonell1dawnuria2020@gmail.com>
2022-05-30 16:44:31 +0200

    user update

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -11,34 +11,39 @@
         </div>
         <input type="file" name="Cambiar" id="">
     </div>
-    <div class="editar_perfil">
-        <div class="correo">
-            <input type="email" id="email" name="correo" placeholder="asasa">
-        </div>
-        <div class="user">
-            <input type="user" id="user" name="user" placeholder="Usuario">
-        </div>
-        <div class="new_password">
-            <input type="password" id="new_password" name="new_password" placeholder="Nueva contraseña">
-        </div>
-        <div class="confirm_password">
-            <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirmar contraseña">
-        </div>
-        <div class="info">
-            <h1>Introduce tu contraseña actual para guardar<br> los cambios:</h1>
-        </div>
-        <div class="curent_password">
-            <input type="password" id="curentpassword" name="curentpassword" placeholder="Contraseña actual">
-        </div>
+    <form method="POST" action="{{ route('user.update', Auth::user()->id) }}">
+        @method('PUT')
+        @csrf
 
-        <div class="btn_save_perfil">
-            <button type="btn_save_perfil">Guardar perfil</button>
-        </div>
-        @auth
-            <div id="userid" hidden>
-                {{ Auth::user()->id }}
+        <div class="editar_perfil">
+            <div class="correo">
+                <input type="email" id="email" name="email" placeholder="Email">
             </div>
-        @endauth
+            <div class="user">
+                <input type="user" id="user" name="user" placeholder="Usuario">
+            </div>
+            <div class="new_password">
+                <input type="password" id="new_password" name="new_password" placeholder="Nueva contraseña">
+            </div>
+            <div class="confirm_password">
+                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirmar contraseña">
+            </div>
+            <div class="info">
+                <h1>Introduce tu contraseña actual para guardar<br> los cambios:</h1>
+            </div>
+            <div class="curent_password">
+                <input type="password" id="curentpassword" name="curentpassword" placeholder="Contraseña actual">
+            </div>
+            @auth
+                <div id="userid" name="userid" hidden>
+                    <input type="text" id="userid" name="userid" value="{{ Auth::user()->id }}">
+                </div>
+            @endauth
+
+            <div class="btn_save_perfil">
+                <button type="submit">Guardar perfil</button>
+            </div>
+    </form>
     </div>
     <script>
         const userid = document.getElementById("userid");
