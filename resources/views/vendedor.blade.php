9824a9c115ad8a27239508ea585467f975626655
Joanecito <j.perez1dawnuria2020@gmail.com>
2022-05-31 17:46:12 +0200

    separar inventario i en venta

--- resources/views/vendedor.blade.php
+++ resources/views/vendedor.blade.php
@@ -19,10 +19,12 @@
             </div>
 
             <div id="inventario"></div>
+            <div id="enventa"></div>
         </div>
     </div>
 
     <script>
+
         const vendedores = document.getElementById("vendedor");
         const mostrarban = document.getElementById("mostrarban");
         const inventario = document.getElementById("inventario");
@@ -105,6 +107,95 @@
                 });
             }
         }
+        async function onClickVenta(){
+            const venta = document.getElementsByClassName("venta")[0];
+            const name = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+            const response = await fetch('{{ env('APP_URL') }}' + `/api/users/${name}`)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => data)
+                .catch(err => err)
+                venta.style.background="#282B2F";
+                venta.style.color = "white";
+                inventario_vendor.style.background = "none";
+                inventario_vendor.style.color = "#282B2F";
+
+                inventario.innerHTML = "";
+            response.nfts.map((nft) => {
+                if(nft.onStock == 1){
+                    inventario.innerHTML += `
+                <div class="vendor_card-nft" id="${nft.id}">
+                    <div class="card-image" id="${nft.id}">
+                        <img id="${nft.id}" src="{{ asset('storage/${nft.photo}') }}">
+                    </div>
+                    <div id="${nft.id}" class="card-info">
+                        <p class="card-title">${nft.title}</p>
+                        <div id="${nft.id}" class="card-username">
+                            <div class="card-photouser">
+                                <img src="{{ asset('storage/${response.user.photo}') }}">
+                            </div>
+                            <div class="card-name">${response.user.name}</div>
+                            <div class="card-price">
+                                ${nft.price}€
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+                }
+                
+            });
+            
+        }
+        async function onClickInventario(){
+            const inventario_vendor = document.getElementsByClassName("inventario_vendor")[0];
+            const name = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+            const response = await fetch('{{ env('APP_URL') }}' + `/api/users/${name}`)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => data)
+                .catch(err => err)
+                inventario_vendor.style.color="white"
+                inventario_vendor.style.background="#282B2F";
+                venta.style.background = "none";
+                venta.style.color = "#282B2F";
+                inventario.innerHTML = "";
+            response.nfts.map((nft) => {
+                    inventario.innerHTML += `
+                <div class="vendor_card-nft" id="${nft.id}">
+                    <div class="card-image" id="${nft.id}">
+                        <img id="${nft.id}" src="{{ asset('storage/${nft.photo}') }}">
+                    </div>
+                    <div id="${nft.id}" class="card-info">
+                        <p class="card-title">${nft.title}</p>
+                        <div id="${nft.id}" class="card-username">
+                            <div class="card-photouser">
+                                <img src="{{ asset('storage/${response.user.photo}') }}">
+                            </div>
+                            <div class="card-name">${response.user.name}</div>
+                            <div class="card-price">
+                                ${nft.price}€
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+                
+            });
+            
+        }
+        const inventario_vendor = document.getElementsByClassName("inventario_vendor")[0];
+        const venta = document.getElementsByClassName("venta")[0];
+        venta.addEventListener("click",(e)=>{
+                console.log("venta");
+                onClickVenta();
+            })
+            inventario_vendor.addEventListener("click",(e)=>{
+                console.log("inventario_vendor");
+                onClickInventario();
+            })
         window.onload = getUserData();
     </script>
 @endsection
--- resources/views/vender.blade.php
+++ resources/views/vender.blade.php
@@ -1,67 +1,72 @@
 @extends('layouts.app')
 @section('content')
-    <div id="nftindividual">
-        <div class="headercomprar">Comprar</div>
-        <div id="caracteristicasnft">
-            <div id="nft-title">
-                <div class="arrowback">
-                    <-< /div>
-                        <div class="titlenft">NFT Caracteristicas</div>
-                </div>
-                <div id="nft-image">
-                    <img src="{{ asset('img/Fotonftexample.png') }}">
-                </div>
-                <div id="nft-information">
-                    <div id="nft-username">
-                        <div id="nft-usertitle">Cool Nft Devep...</div>
-                        <div id="nft-photouser">
-                            <div id="nft-photo">
-                                <img src="{{ asset('img/sylvia.png') }}">
-                            </div>
-                            <div id="nft-name">Skyler Dias</div>
+<div id="nftindividual">
+    <div class="headercomprar">Comprar</div>
+    <div id="caracteristicasnft">
+        <div id="nft-title">
+            <div class="arrowback">
+                <-< /div>
+                    <div class="titlenft">NFT Caracteristicas</div>
+            </div>
+            <div id="nft-image">
+                <img src="{{ asset('img/Fotonftexample.png') }}">
+            </div>
+            <div id="nft-information">
+                <div id="nft-username">
+                    <div id="nft-usertitle">Cool Nft Devep...</div>
+                    <div id="nft-photouser">
+                        <div id="nft-photo">
+                            <img src="{{ asset('img/sylvia.png') }}">
                         </div>
-                        <div id="headercomprar">Comprar</div>
-                        <div id="backcompra">
-                            <- NFT Características</div>
-                                <div id="comprarnft">
-                                    <div id="imgnft">
+                        <div id="nft-name">Skyler Dias</div>
+                    </div>
+                    <div id="headercomprar">Comprar</div>
+                    <div id="backcompra">
+                        <- NFT Características</div>
+                            <div id="comprarnft">
+                                <div id="imgnft">
 
+                                </div>
+                                <div id="compra">
+                                    <div id="venededorusername">
                                     </div>
-                                    <div id="compra">
-                                        <div id="venededorusername">
-                                        </div>
-                                        <div id="vendedordescription">
-                                            Me encanta este nft super curioso y grande, por eso lo vendo a un precio muy
-                                            asequible.
-                                        </div>
-                                        <div id="pricefinal">
-                                            <div id="price">Calculando</div>
-                                            <div id="botonvender">vender</div>
-                                        </div>
-
+                                    <div id="vendedordescription">
+                                        Me encanta este nft super curioso y grande, por eso lo vendo a un precio muy
+                                        asequible.
+                                    </div>
+                                    <div id="pricefinal">
+                                        <div id="price">Calculando</div>
+                                        <div id="botonvender">vender</div>
                                     </div>
 
                                 </div>
-                                <script>
-                                    const comprar = document.getElementById("botoncomprar");
+                                @auth
+                                <div hidden>
+                                    <input hidden type="text" id="userid" name="userid" value="{{ Auth::user()->id }}">
+                                </div>
+                                @endauth
+                            </div>
+                            <script>
+                                const userid = document.getElementById("userid");
+                                const comprar = document.getElementById("botoncomprar");
 
-                                    comprar.addEventListener("click", () => {
-                                        console.log("works");
-                                        const div = document.createElement("div");
-                                        div.id = "confirmation";
+                                comprar.addEventListener("click", () => {
+                                    console.log("works");
+                                    const div = document.createElement("div");
+                                    div.id = "confirmation";
 
-                                        document.body.appendChild(div);
-                                        addCard();
-                                    });
-                                    document.body.addEventListener("mousedown", (e) => {
-                                        if (e.target.id === "confirmation") {
-                                            e.target.remove();
-                                        }
-                                    });
+                                    document.body.appendChild(div);
+                                    addCard();
+                                });
+                                document.body.addEventListener("mousedown", (e) => {
+                                    if (e.target.id === "confirmation") {
+                                        e.target.remove();
+                                    }
+                                });
 
-                                    function addCard() {
-                                        const confirmation = document.getElementById("confirmation");
-                                        confirmation.innerHTML += `
+                                function addCard() {
+                                    const confirmation = document.getElementById("confirmation");
+                                    confirmation.innerHTML += `
                                             <div id="creditcard">
                                                 <img src="{{ asset('img/Fotonftexample.png') }}"></img>
                                                 <div id="namecreditcard">
@@ -81,64 +86,64 @@
                                                 <div id="nftconfirmar" onclick="creditcardAccepted()">Confirmar</div>
                                             </div>
                                         `;
-                                    }
-                                    async function comprarNFT() {
-                                        console.log("funciona");
-                                        console.log("-----------------------------");
-                                        const id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
-                                        const response = await fetch(`{{ env('APP_URL') }}/api/operations/5/transaction`)
-                                            .then(res => res.json())
-                                            .then(data => data)
-                                            .catch(err => err)
-                                        console.log(response);
-                                    }
+                                }
+                                async function comprarNFT() {
+                                    console.log("funciona");
+                                    console.log("-----------------------------");
+                                    const id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+                                    const response = await fetch(`{{ env('APP_URL') }}/api/operations/${userid.value}/transaction`)
+                                        .then(res => res.json())
+                                        .then(data => data)
+                                        .catch(err => err)
+                                    console.log(response);
+                                }
 
-                                    function creditcardAccepted() {
-                                        console.log("works");
-                                        comprarNFT();
+                                function creditcardAccepted() {
+                                    console.log("works");
+                                    comprarNFT();
 
-                                        document.getElementById("confirmation").remove();
-                                        const div = document.createElement("div");
-                                        div.id = "confirmation";
+                                    document.getElementById("confirmation").remove();
+                                    const div = document.createElement("div");
+                                    div.id = "confirmation";
 
-                                        document.body.appendChild(div);
-                                        successfulWindow();
-                                    }
+                                    document.body.appendChild(div);
+                                    successfulWindow();
+                                }
 
-                                    function successfulWindow() {
-                                        const confirmation = document.getElementById("confirmation");
-                                        confirmation.innerHTML += `
+                                function successfulWindow() {
+                                    const confirmation = document.getElementById("confirmation");
+                                    confirmation.innerHTML += `
                                             <div id="successful">
                                                 <img src="{{ asset('img/Group.png') }}"></img>
                                                 <p>¡Enhorabuena has conseguido un nuevo NFT!</p>
                                                 <div><a href="/mercado">Ir al mercado</a></div>
                                             </div>
                                         `;
-                                    }
-                                    const getNFTIndividual = async () => {
-                                        const id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
-                                        const imgnft = document.getElementById("imgnft");
-                                        const vendedorusername = document.getElementById("venededorusername");
-                                        const vendedordescription = document.getElementById("vendedordescription");
-                                        const price = document.getElementById("price");
-                                        const confirmar = document.getElementById("nftconfirmar");
-                                        const response = await fetch(`{{ env('APP_URL') }}/api/nfts/${id}:8000`)
-                                            .then(res => {
-                                                return res.json();
-                                            })
-                                            .then(data => data)
-                                            .catch(err => err)
-                                        imgnft.innerHTML += `
+                                }
+                                const getNFTIndividual = async () => {
+                                    const id = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+                                    const imgnft = document.getElementById("imgnft");
+                                    const vendedorusername = document.getElementById("venededorusername");
+                                    const vendedordescription = document.getElementById("vendedordescription");
+                                    const price = document.getElementById("price");
+                                    const confirmar = document.getElementById("nftconfirmar");
+                                    const response = await fetch(`{{ env('APP_URL') }}/api/nfts/${id}/putOnStock`)
+                                        .then(res => {
+                                            return res.json();
+                                        })
+                                        .then(data => data)
+                                        .catch(err => err)
+                                    imgnft.innerHTML += `
                                             <img src="{{ asset('storage/${response.data[0].photo}') }}"></img>
                                         `;
-                                        vendedorusername.innerHTML += `
+                                    vendedorusername.innerHTML += `
                                             <img src="{{ env('APP_URL') }}/img/sylvia.png">
                                                 Username
                                         `;
-                                        vendedordescription.innerHTML = response.data[0].description;
-                                        price.innerHTML = response.data[0].price + ' €';
-                                        console.log(response);
-                                    }
-                                    getNFTIndividual();
-                                </script>
+                                    vendedordescription.innerHTML = response.data[0].description;
+                                    price.innerHTML = response.data[0].price + ' €';
+                                    console.log(response);
+                                }
+                                getNFTIndividual();
+                            </script>
                             @endsection
