0a3b498d19a870c7d8ca7efb76ce28623b2f949c
xcarbonell <x.carbonell1dawnuria2020@gmail.com>
2022-05-31 20:19:17 +0200

    god pls

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,11 +26,7 @@
             <li tabindex="0"><img src="{{ asset('img/Home.png') }}"><a href="/" alt="Imagen de inicio">Inicio</a></li>
             <li tabindex="0"><img src="{{ asset('img/Bag.png') }}"><a href="/mercado" alt="Imagen del mercado">Mercado</a></li>
             @guest
-<<<<<<< HEAD
-            <li><img src="{{ asset('img/Logout.png') }}"></img><a href="/login">Acceso</a></li>
-=======
                 <li tabindex="0"><img src="{{ asset('img/Logout.png') }}" alt="Imagen para desloguearse"><a href="/login">Acceso</a></li>
->>>>>>> cccaf1b5dacf16d37d49f8859831c1ffdd14ee70
             @endguest
         </ul>
         <ul>
@@ -45,16 +41,6 @@
             @endauth
         </ul>
         @auth
-<<<<<<< HEAD
-        <div class="logout">
-            <a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                @csrf
-            </form>
-        </div>
-=======
             <div class="logout">
                 <a class="logout" href="{{ route('logout') }}"
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();" tabindex="0">
@@ -64,7 +50,6 @@
                     @csrf
                 </form>
             </div>
->>>>>>> cccaf1b5dacf16d37d49f8859831c1ffdd14ee70
         @endauth
     </nav>
     <div id="navmenu" tabindex="0">
@@ -74,35 +59,6 @@
                 <li><a href="/" tabindex="0"><img src="{{ asset('img/Home.png') }}">Inicio</a></li>
                 <li><a href="/mercado" tabindex="0"><img src="{{ asset('img/Bag.png') }}">Mercado</a></li>
                 @guest
-<<<<<<< HEAD
-                <li><a href="/login"><img src="{{ asset('img/Logout.png') }}"></img>Acceso</a></li>
-                @endguest
-            </ul>
-            @auth
-            <ul>
-                <label>Información</label>
-                <li><a href="/perfil"><img src="{{ asset('img/Profile.png') }}"></img>Editar Perfil</a></li>
-                <li><a href="/transacciones"><img src="{{ asset('img/Transaction.png') }}"></img>Transacciones</a>
-                </li>
-                <li><a href="/inventario"><img src="{{ asset('img/Bookmark.png') }}"></img>Inventario</a></li>
-                @auth
-                <li id="mitienda"><a href="{{ env('APP_URL') }}/users/{{Auth::user()->name}}"><img src="{{ asset('img/store.png') }}"></img>Tienda</a></li>
-                @endauth
-                @auth
-                @if(Auth::user()->role_id == 1)
-                <li><a href="/facturacion"><img src="{{ asset('img/wallet.png') }}"></img>Facturación</a></li>
-                @endif
-                @endauth
-            </ul>
-            <div class="logout">
-                <a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                    @csrf
-                </form>
-            </div>
-=======
                     <li><a href="/login" tabindex="0"><img src="{{ asset('img/Logout.png') }}">Acceso</a></li>
                 @endguest
             </ul>
@@ -128,7 +84,6 @@
                         @csrf
                     </form>
                 </div>
->>>>>>> cccaf1b5dacf16d37d49f8859831c1ffdd14ee70
             @endauth
         </nav>
     </div>
